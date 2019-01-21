ec5523944a5202fb4959c41c8ae2e833e514cfb1
Camila <salescamila1@gmail.com>
2019-01-20 23:17:51 -0400

    fix: Add list books title

--- src/listBooks.js
+++ src/listBooks.js
@@ -10,30 +10,35 @@ function ListBooks({ books, onOpenSearch, onChangeShelf }) {
   const shelfRead = books.filter((book) => {return book.shelf === 'read'})
 
   return(
-    <div className="list-books-content">
-      <BookShelf
-        shelfTitle={'Currently Reading'}
-        books={shelfReading}
-        onChangeShelf={(book, shelf) => (
-          onChangeShelf(book, shelf)
-        )}
-        />
-      <BookShelf
-        shelfTitle={'To Read'}
-        books={shelfToRead}
-        onChangeShelf={(book, shelf) => (
-          onChangeShelf(book, shelf)
-        )}
-        />
-      <BookShelf
-        shelfTitle={'Read'}
-        books={shelfRead}
-        onChangeShelf={(book, shelf) => (
-          onChangeShelf(book, shelf)
-        )}
-        />
-      <div className="open-search">
-        <button onClick={() => onOpenSearch()}>Add a book</button>
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <BookShelf
+          shelfTitle={'Currently Reading'}
+          books={shelfReading}
+          onChangeShelf={(book, shelf) => (
+            onChangeShelf(book, shelf)
+          )}
+          />
+        <BookShelf
+          shelfTitle={'To Read'}
+          books={shelfToRead}
+          onChangeShelf={(book, shelf) => (
+            onChangeShelf(book, shelf)
+          )}
+          />
+        <BookShelf
+          shelfTitle={'Read'}
+          books={shelfRead}
+          onChangeShelf={(book, shelf) => (
+            onChangeShelf(book, shelf)
+          )}
+          />
+        <div className="open-search">
+          <button onClick={() => onOpenSearch()}>Add a book</button>
+        </div>
       </div>
     </div>
   )
